3ea501fdf3274fdd880661b36320e6febf72b46b
Aidar <abdykayimovaidar@gmail.com>
2023-11-26 15:13:48 +0300

    device height fix

--- src/pageblock/WelcomePage/WelcomePage.tsx
+++ src/pageblock/WelcomePage/WelcomePage.tsx
@@ -4,11 +4,11 @@ type Props = {}
 
 export const WelcomePage = (props: Props) => {
     return (
-        <div className="min-h-screen text-black w-[100vw] flex flex-col items-center justify-center">
+        <div className=" h-[100dvh] text-black w-[100vw] flex flex-col items-center justify-center bg-cover bg-[url('/images/pattern2.png')] ">
             <div>
-                <h1 className='text-clamp font-bold text-shadow-pop-bottom'>AIDAR.</h1>
+                <h1 className='text-clamp font-bold'>AIDAR.</h1>
             </div>
-            <div className='flex justify-around w-full text-4xl'>
+            <div className='flex justify-around w-full text-4xl md:flex-row flex-col text-center'>
                 <p>Frontend Developer</p>
                 <p>UI/UX Designer</p>
             </div>
